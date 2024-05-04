1e0ef058b35b9ad7ae78eec3a9c767dcf57aa49f
amrayi <amirmohammadamrayi@gmail.com>
2024-05-04 12:43:15 +0330

    linked articles page

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = "blog"
 urlpatterns = [
     path('', home, name="home"),
     path('article/<slug:slug>', detail, name="detail"),
-    path('catefory/<slug:slug>', category , name="category")
+    path('category/<slug:slug>', category , name="category")
 ]
